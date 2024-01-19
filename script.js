ed28c4c2ac9071cb285dd52937279849b3611d2e
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2024-01-19 17:42:48 +0600

    Done

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const textElement = document.getElementById('typewriter-text');
-    const textContent = textElement.innerHTML;
-    textElement.innerHTML = '';
+// document.addEventListener('DOMContentLoaded', function () {
+//     const textElement = document.getElementById('typewriter-text');
+//     const textContent = textElement.innerHTML;
+//     textElement.innerHTML = '';
 
-    function typeWriter() {
-        let i = 0;
-        const speed = 30; // Adjust the typing speed (milliseconds per character)
+//     function typeWriter() {
+//         let i = 0;
+//         const speed = 30; // Adjust the typing speed (milliseconds per character)
 
-        function type() {
-            if (i < textContent.length) {
-                textElement.innerHTML += textContent.charAt(i);
-                i++;
-                setTimeout(type, speed);
-            }
-        }
+//         function type() {
+//             if (i < textContent.length) {
+//                 textElement.innerHTML += textContent.charAt(i);
+//                 i++;
+//                 setTimeout(type, speed);
+//             }
+//         }
 
-        type();
-    }
+//         type();
+//     }
 
-    typeWriter();
+//     typeWriter();
 
-    function scrollText() {
-        const container = document.getElementById('typewriter-container');
-        container.scrollTop = container.scrollHeight;
-    }
+//     function scrollText() {
+//         const container = document.getElementById('typewriter-container');
+//         container.scrollTop = container.scrollHeight;
+//     }
 
-    setInterval(scrollText, 3000); // Adjust the interval for scrolling (milliseconds)
-});
+//     setInterval(scrollText, 3000); // Adjust the interval for scrolling (milliseconds)
+// });
+
+
+
